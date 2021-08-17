d142230f27300aa50d778bdbae1c3372539ae5d6
Clement Okyere <clement.okyere18@gmail.com>
2021-08-17 17:35:57 +0000

    added function to pull data using database pool, query and variables

--- src/utils/dbConn.js
+++ src/utils/dbConn.js
@@ -8,3 +8,30 @@ export const bookDbPool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
 });
+
+
+/**
+ * 
+ * @param {pg database pool} pool 
+ * @param {sql query} sql 
+ * @param {query parameters} variables 
+ * @returns 
+ */
+export const cquery = async (pool, sql, variables = null) => {
+  const client = await pool.connect();
+  let res;
+  if (variables !== null && variables.constructor === Array) {
+    const query = {
+      text: sql,
+      values: variables,
+    };
+    res = await client.query(query, variables);
+  } else {
+    res = await client.query(sql);
+  }
+
+  //releases databases resources after fetching data
+  client.release();
+  const data = res.rows || [];
+  return data;
+};
