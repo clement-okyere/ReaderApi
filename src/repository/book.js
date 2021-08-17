1ca9a5db69500178c5ea1f36aa3c72273796b471
Clement Okyere <clement.okyere18@gmail.com>
2021-08-17 18:08:55 +0000

    get unique clients that have read the book

--- src/repository/book.js
+++ src/repository/book.js
@@ -2,17 +2,28 @@ import { bookDbPool } from "../utils/dbConn";
 import { cquery } from "../utils/dbConn";
 
 class BookRepository {
-    static async getBooks(limit, offset) {
-        const queryString = `
+  static async getBooks(limit, offset) {
+    const queryString = `
           SELECT uuid, title, author, language
           FROM book
           ORDER BY id 
           LIMIT $1 OFFSET $2;
         `;
 
-        let result = await cquery(bookDbPool, queryString, [limit, offset]);
-        return result;
-    }
+    let result = await cquery(bookDbPool, queryString, [limit, offset]);
+    return result;
+  }
+
+  static async getUniqueClients(bookId) {
+    const queryString = `
+         SELECT COUNT(distinct (client_id))
+             from log
+            WHERE book_id = $1
+        `;
+
+    let result = await cquery(bookDbPool, queryString, [bookId]);
+    return result;
+  }
 }
 
 export default BookRepository;
