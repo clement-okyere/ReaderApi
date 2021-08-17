23517475795f8cf2de9fce26cf9139a442f3641a
Clement Okyere <clement.okyere18@gmail.com>
2021-08-17 18:30:29 +0000

    added integration tests for book statistics

--- src/test/integration/book.test.js
+++ src/test/integration/book.test.js
@@ -24,4 +24,21 @@ describe("api/books", () => {
       expect(Array.isArray(res.body)).toBe(true);
     });
   });
+
+//   Book statistics take too long to fetch.
+//   Logs table has to be optmized
+
+    describe("GET /:id/statistics", () => {
+     it("should return book statistics", async () => {
+       const bookId = "54281c97-e81e-4694-bcb9-28d3ac0925a5";
+         const res = await request(server)
+             .get(`/api/books/${bookId}/statistics`)
+             .timeout(300000);
+
+       expect(res.status).toBe(200);
+       expect(res.body).toHaveProperty("uniqueClients");
+       expect(res.body).toHaveProperty("uniqueUsers");
+       expect(res.body).toHaveProperty("clientCountrySummary");
+     }, 300000);
+  });
 });
