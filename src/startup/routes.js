edb3333d179d194f54cb8e664fbf3f43e2e34c83
Clement Okyere <clement.okyere18@gmail.com>
2021-08-17 18:43:40 +0000

    added endpoint route middleware

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -1,9 +1,11 @@
 import books from "../route/book";
+import category from "../routes/category";
 import express from "express";
 
 const routes = (app) => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use("/api/books", books);
+  app.use("/api/categories", category);
 };
 export default routes;
